bdd0c83f52fc1be98ece6e719051e454e6252f6f
Taimoor Ali <taimoorali4214@gmail.com>
2024-02-04 18:13:45 +0500

    checkbox added to questions

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,6 +5,17 @@ export default function Search({ data }) {
   const [searchText, setSearchText] = useState("");
   const [currentData, setCurrentData] = useState(data);
 
+  const [seleted, setSelected] = useState([]);
+
+  const handelSelect = (question) => {
+    const existing = seleted.find((q) => q === question);
+    if (!existing) {
+      setSelected((prev) => [...prev, question]);
+    } else {
+      setSelected((prev) => prev.filter((q) => q !== question));
+    }
+  };
+
   useEffect(() => {
     if (!searchText) {
       setCurrentData(data);
@@ -14,6 +25,7 @@ export default function Search({ data }) {
   }, [searchText]);
   useEffect(() => {
     setCurrentData(data);
+    setSelected([]);
   }, [data]);
 
   return (
@@ -32,9 +44,12 @@ export default function Search({ data }) {
       <div className="bg-white p-2 flex flex-col gap-1 font-medium capitalize flex-1 overflow-x-hidden overflow-y-auto">
         {currentData.length > 0 ? (
           currentData.map((question) => (
-            <div className="bg-neutral-100 p-1 rounded" key={question}>
-              {question}
-            </div>
+            <Question
+              key={question}
+              question={question}
+              handelSelect={handelSelect}
+              seleted={seleted}
+            />
           ))
         ) : (
           <div className="py-2 text-center">No match</div>
@@ -43,3 +58,25 @@ export default function Search({ data }) {
     </div>
   );
 }
+
+const Question = ({ question, handelSelect, seleted }) => {
+  function status() {
+    const exist = seleted.find((i) => i === question);
+    return exist ? true : false;
+  }
+  return (
+    <div
+      className="bg-neutral-100 p-1 rounded flex items-center gap-2 cursor-pointer"
+      onClick={() => handelSelect(question)}
+      key={question}
+    >
+      <input
+        type="checkbox"
+        className="accent-gray-500"
+        readOnly
+        checked={status()}
+      />
+      {question}
+    </div>
+  );
+};
